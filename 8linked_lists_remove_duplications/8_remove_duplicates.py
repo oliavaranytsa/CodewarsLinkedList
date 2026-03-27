afed2c5bb1c9dda8ce604c95e5c422d1cce4147d
Olga Varanytsia <varanytsa.pn@ucu.edu.ua>
2026-03-27 11:56:07 +0200

    remove duplicate elements from sorted linked list

---
+++ 8linked_lists_remove_duplications/8_remove_duplicates.py
@@ -0,0 +1,11 @@
+def remove_duplicates(head):
+    if head is None:
+        return None
+
+    current = head
+    while current.next is not None:
+        if current.data == current.next.data:
+            current.next = current.next.next
+        else:
+            current = current.next
+    return head
